cb6d44300f609340d2fea0adb870c2ccd03ae324
Aswin C <aswinanand9020@gmail.com>
2025-12-29 22:24:07 +0530

    Auto Update Mon Dec 29 22:24:07 IST 2025

--- src/App.tsx
+++ src/App.tsx
@@ -423,7 +423,7 @@ export default function App(): React.ReactElement {
                 }}
                 onLogoClick={() => window.location.href = 'https://theboxcric.web.app/?match=inwslw12282025268163&forceLive=true'}
                 data={headerData}
-                isLive={selectedMatch?.event_state === 'L'}
+                isLive={matches.some(m => m.event_state === 'L') || selectedMatch?.event_state === 'L'}
             />
 
             {/* Main Content */}
